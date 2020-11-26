ca849a56720dd817d131eef8315e9a3b6b757cb0
Krishnaraj P Thadesar <kpt.krishnaraj@gmail.com>
2020-11-26 21:16:41 +0530

    Added Test Graphs in the Progress Images

--- Graphs.py
+++ Graphs.py
@@ -3,10 +3,10 @@
 import File_IO as fio
 import matplotlib.pyplot as plt
 
-video_ratings = fio.read.get_ratings()
+video_ratings = fio.read.get_views()
 
-plt.xlabel('Range')
-plt.ylabel('Rating')
+plt.xlabel('Videos')
+plt.ylabel('views')
 
 plt.bar(range(24), video_ratings)
 plt.show()
